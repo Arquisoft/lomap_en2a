2d91bd19bff25435269132f065ef27d90ff20fe0
Patri <uo282210@uniovi.es>
2023-03-11 20:42:13 +0100

    profile image change (know is default avatar)

--- webapp/src/components/ProfileInfo.tsx
+++ webapp/src/components/ProfileInfo.tsx
@@ -1,4 +1,4 @@
-import { Image, Button, Text, Flex, VStack, Box } from "@chakra-ui/react"
+import { Avatar, Button, Text, Flex, VStack, Box } from "@chakra-ui/react"
 import React from "react"
 
 export function ProfileView() {  
@@ -14,18 +14,16 @@ export function ProfileView() {
         zIndex={1}
         overflow='hidden'
         px={2}>
-        <Text fontSize='1.2em' borderBottomWidth='1px'>Profile Information</Text>
+        <Text fontSize='1.2em' borderBottomWidth='1px' margin={'20px'}>Profile Information</Text>
         <VStack>
-            <Image
-            borderRadius='full' //redondeados
-            boxSize='150px'
-            src='https://bit.ly/dan-abramov' // imagen por defecto
-            alt='Dan Abramov'
-            />
-            <p style={{"fontSize":"1.2em"}}>Dan Abramov</p>
-          </VStack>
+            <Avatar 
+            marginTop={'20px'}
+            bg='red.500'
+            size='xl'/>
+          <Text fontSize='1.2em' as="b">Dan Abramov</Text>
+        </VStack>
           <Box p={2} shadow='md' borderWidth='1px'>
-            <Text as="b">User information</Text>
+            <Text as="b">Statistics</Text>
             <Text>198 Locations</Text>
             <Text>199987624 Reviews</Text>
           </Box>
