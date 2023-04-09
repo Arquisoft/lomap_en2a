1891ba3c5893177e02495f82f776962f5f02c502
UO283216 <98907723+UO283216@users.noreply.github.com>
2023-04-10 00:20:02 +0200

    Menu tests completed

--- webapp/src/components/Menu.test.tsx
+++ webapp/src/components/Menu.test.tsx
@@ -9,7 +9,7 @@ test('check menu contains 5 options',async () => {
 })
 
 test('check menu expands when mouse enters', async () => {
-    const { getByTestId } = render(
+    const { getByTestId } = render( 
       <Menu addLocation={jest.fn()} deleteLoc={jest.fn()} locations={[]} setSelectedView={jest.fn()} />
     );
     //we expect the small component to be in the document
@@ -19,3 +19,50 @@ test('check menu expands when mouse enters', async () => {
     // wait for the component to update
     expect(getByTestId('bigContainer')).toBeInTheDocument()
   });
+
+test('check menu shrinks when mouse exits', async () => {
+    const { getByTestId } = render( 
+        <Menu addLocation={jest.fn()} deleteLoc={jest.fn()} locations={[]} setSelectedView={jest.fn()} />
+    );
+    // enter the menu with the mouse
+    fireEvent.mouseEnter(getByTestId('smallContainer'));
+
+    //we expect the big component to be in the document
+    expect(getByTestId('bigContainer')).toBeInTheDocument()
+    // exit the menu with the mouse
+    fireEvent.mouseLeave(getByTestId('bigContainer'));
+    // wait for the component to update
+    expect(getByTestId('smallContainer')).toBeInTheDocument()
+});
+
+test.each(['Map view', 'Location list', 'Add location','Add friends','Profile'])(
+    'clicking %s updates the view',
+    async (buttonText) => {
+      let viewUpdated = false;
+      const { getByTestId, getAllByRole } = render(
+        <Menu
+          addLocation={jest.fn()}
+          deleteLoc={jest.fn()}
+          locations={[]}
+          setSelectedView={(view) => {
+            viewUpdated = true;
+          }}
+        />
+      );
+      // enter the menu with the mouse
+      fireEvent.mouseEnter(getByTestId('smallContainer'));
+      // get the button by its text content
+      const button = getAllByRole('button', { name: buttonText })[0];
+      expect(getByTestId('bigContainer')).toBeInTheDocument();
+      
+      // click the button
+      button.click();
+      // expect the view to have been updated
+      expect(viewUpdated).toBe(true);
+      // enter the menu with the mouse
+      fireEvent.mouseEnter(getByTestId('smallContainer'));
+    }
+  );
+  
+
+
