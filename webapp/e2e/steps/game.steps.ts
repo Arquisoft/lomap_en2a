2d1508f2379f7abda9fb094b4ffa77778a13833d
Patri <uo282210@uniovi.es>
2023-05-02 01:38:05 +0200

    cucumber passing

--- webapp/e2e/steps/game.steps.ts
+++ webapp/e2e/steps/game.steps.ts
@@ -37,16 +37,17 @@ defineFeature(feature, test => {
 
             await page.waitForNavigation(); // wait for the redirect
             // await page.waitForTimeout(30000); // wait for 25 seconds (load locations??)
+            await expect(page).toClick("button", {text:"Close"});
             await page.waitForTimeout(8000);
             
-            const [menu] = await page.$x('/html/body/div[1]/div/div[2]/div');
+            const [menu] = await page.$x('//*[@id="smallContainer"]');
             await menu.click();
             await page.waitForTimeout(3000)
         });
 
         when("The user clicks the game button", async () => {
-            const [profile] = await page.$x('/html/body/div[1]/div/div[2]/div/div[5]/button')
-            await profile.click();
+            const [game] = await page.$x('/html/body/div[1]/div/div[3]/div/div[5]/button')
+            await game.click();
             await page.waitForTimeout(3000); // wait for 10 seconds
 
         });
